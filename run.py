a714a86f085dd19b7b46d75abd0ed30fce966e7f
Favarete <rodrigo@favarete.com>
2019-04-25 14:14:31 -0300

    Create class abstration

--- run.py
+++ run.py
@@ -1,17 +1,39 @@
 import urwid
+import time
+import sys
+from random import randint
 
-def show_or_exit(key):
-    if key in ('q', 'Q'):
-        raise urwid.ExitMainLoop()
-    return key
+class View():
 
-counter = urwid.BigText(('banner', "4"), urwid.Thin3x3Font())
+    def __init__(self):
+        self.view = self.setup_view(self.default_widgets())
+    
+    def keyboard(self, key):
+        if key in ('q', 'Q'):
+            raise urwid.ExitMainLoop()
+        return key
 
-view = urwid.ListBox(urwid.SimpleFocusListWalker([
-    urwid.Text(u'TEXT', align='center'),
-    urwid.Padding(counter, width='clip', align='center'),
-    urwid.Text(u'TEXT', align='center')
-]))
+    def setup_view(self, widgets):
+        return urwid.ListBox(urwid.SimpleFocusListWalker(widgets))
 
-loop = urwid.MainLoop(view, unhandled_input=show_or_exit)
+    def default_widgets(self):
+        body = urwid.Text(u'INIT', align='center')
+        return [body]
+
+    def mount_widgets(self):
+        header = urwid.Text(u'TEXT', align='center')
+        _timer_text = urwid.BigText(str(randint(1,50)), urwid.Thin3x3Font())
+        body = urwid.Padding(_timer_text, width='clip', align='center')
+        footer = urwid.Text(u'TEXT', align='center')
+        return [header, body, footer]
+
+    def refresh(self, loop, param):
+        self.view = self.setup_view(self.mount_widgets())
+        loop.widget = self.view
+        loop.set_alarm_in(1, self.refresh)
+
+
+screen = View()
+loop = urwid.MainLoop(screen.view, unhandled_input=screen.keyboard)
+loop.set_alarm_in(1, screen.refresh, user_data=None)
 loop.run()
