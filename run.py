38ac3d9dd925f1c51ecfe18bdc85a9d9b3d41bf7
Favarete <rodrigo@favarete.com>
2019-04-25 05:10:58 -0300

    Add application visual fundaments

---
+++ run.py
@@ -0,0 +1,16 @@
+import urwid
+def show_or_exit(key):
+    if key in ('q', 'Q'):
+        raise urwid.ExitMainLoop()
+    return key
+
+counter = urwid.BigText(('banner', "4"), urwid.Thin3x3Font())
+
+view = urwid.ListBox(urwid.SimpleFocusListWalker([
+    urwid.Text(u'TEXT', align='center'),
+    urwid.Padding(counter, width='clip', align='center'),
+    urwid.Text(u'TEXT', align='center')
+]))
+
+loop = urwid.MainLoop(view, unhandled_input=show_or_exit)
+loop.run()
